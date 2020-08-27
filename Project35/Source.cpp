58195a68d0a87dceb86a76bcdc823d8e627131c2
BankZKuma <69130573+BankZKuma@users.noreply.github.com>
2020-08-27 18:33:30 +0700

    Update Source.cpp

--- Project35/Source.cpp
+++ Project35/Source.cpp
@@ -6,18 +6,18 @@ void swap(int* a, int* b) {
 }
 int main() {
 	int i, j;
-	int number[2];
-	for (i = 0; i < 2; i++) {
+	int number[10];
+	for (i = 0; i < 10; i++) {
 		scanf_s("%d", &number[i]);
 	}
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 2 - i; i++) {
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 9 - i; j++) {
 			if (number[j] > number[j + 1]) {
 				swap(&number[j], &number[j + 1]);
 			}
 		}
 	}
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < 10; i++) {
 		printf("%d\t", number[i]);
 	}
 	return 0;
